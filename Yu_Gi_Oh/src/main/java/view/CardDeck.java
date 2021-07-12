b206dc86636941a377c5b9cf936879ce2579ff03
Ramin <Raminfarahani79@yahoo.com>
2021-07-12 14:37:08 +0430

    create stage and background

--- Yu_Gi_Oh/src/main/java/view/CardDeck.java
+++ Yu_Gi_Oh/src/main/java/view/CardDeck.java
@@ -11,6 +11,12 @@ import java.net.URL;
 public class CardDeck extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-
+        URL scoreBoard = getClass().getResource("/fxml/CardDeck.fxml");
+        Parent root = FXMLLoader.load(scoreBoard);
+        Scene scene = new Scene(root);
+        stage.setTitle("ScoreBoard");
+        root.getStyleClass().add("button");
+        stage.setScene(scene);
+        stage.show();
     }
 }
