f1aa77e4b09f55432b41a5f86792171fc9f102ae
Ramin <Raminfarahani79@yahoo.com>
2021-07-12 11:28:33 +0430

    Score board fxml

--- Yu_Gi_Oh/src/main/java/view/ScoreBoard.java
+++ Yu_Gi_Oh/src/main/java/view/ScoreBoard.java
@@ -1,11 +1,25 @@
 package view;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class ScoreBoard extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        URL scoreBoard = getClass().getResource("fxml/ScoreBoard.fxml");
+        Parent root = FXMLLoader.load(scoreBoard);
+        Scene scene = new Scene(root);
+        stage.setTitle("ScoreBoard");
+        stage.setScene(scene);
+        stage.show();
+    }
 
+    public static void main(String[] args) {
+        launch(args);
     }
 }
